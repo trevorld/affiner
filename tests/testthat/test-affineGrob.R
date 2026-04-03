5e28e7903df7085eba071dd0c151ae3426ce64ca
Trevor L. Davis <trevor.l.davis@gmail.com>
2026-04-02 22:43:42 -0700

    tests: Don't leave open x11 window

--- tests/testthat/test-affineGrob.R
+++ tests/testthat/test-affineGrob.R
@@ -1,6 +1,15 @@
 test_that("makeContent.affine() error when device lacks support", {
 	skip_if_not(getRversion() >= "4.2.0")
 	local_mocked_bindings(dev_supports_transformations = function() FALSE)
+	old_dev <- dev.cur()
+	pdf(NULL)
+	on.exit(
+		{
+			dev.off()
+			if (old_dev > 1L) dev.set(old_dev)
+		},
+		add = TRUE
+	)
 	expect_error(
 		grid::grid.draw(affineGrob(grid::nullGrob())),
 		"does not support the affine transformation feature"
@@ -10,10 +19,19 @@ test_that("makeContent.affine() error when device lacks support", {
 test_that("affineGrob() works", {
 	skip_if_not(getRversion() >= "4.3.0")
 	skip_if_not(isTRUE(all(capabilities(c("cairo", "png")))))
+	old_dev <- dev.cur()
 	f <- tempfile(fileext = ".png")
 	png(f, type = "cairo")
+	on.exit(
+		{
+			if (old_dev > 1L) {
+				dev.set(old_dev)
+			}
+			unlink(f)
+		},
+		add = TRUE
+	)
 	grid.affine(grid::nullGrob())
 	dev.off()
 	expect_true(file.size(f) > 0)
-	unlink(f)
 })
